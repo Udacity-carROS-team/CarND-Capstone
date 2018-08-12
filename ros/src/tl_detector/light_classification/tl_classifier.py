e1615dfedf30c0e8256fe99d9511349428b866d4
Tejas Pant <31932990+tejaspant@users.noreply.github.com>
2018-08-12 19:03:51 -0400

    Classification using pure Image Processing

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,4 +1,6 @@
 from styx_msgs.msg import TrafficLight
+import cv2
+import numpy as np
 
 class TLClassifier(object):
     def __init__(self):
@@ -7,13 +9,29 @@ class TLClassifier(object):
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
-
         Args:
             image (cv::Mat): image containing the traffic light
-
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+        """ 
+        image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        lower_red = np.array([0,50,50])
+        upper_red = np.array([10,255,255])
+        red1 = cv2.inRange(image_hsv, lower_red , upper_red)
+        
+        lower_red = np.array([170,50,50])
+        upper_red = np.array([180,255,255])
+        red2 = cv2.inRange(image_hsv, lower_red , upper_red)
+
+        converted_img = cv2.addWeighted(red1, 1.0, red2, 1.0, 0.0)
+        blur_img = cv2.GaussianBlur(converted_img,(15,15),0)
+
+        circles = cv2.HoughCircles(blur_img,cv2.HOUGH_GRADIENT,0.5,41, param1=70,param2=30,minRadius=5,maxRadius=120)
+		
+		#Unknown if Red not detected
+		state = TrafficLight.UNKNOWN
+        if circles is not None:
+            state = TrafficLight.RED
+            
+        return state
